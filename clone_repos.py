2c66107744639f3c410d4b7eb27b2d43f7e68589
Shubham Rooter   -  Shubham Tiwari <94091556+shubham-rooter@users.noreply.github.com>
2023-10-31 19:39:54 +0530

    clone_repos.py

--- clone_repos.py
+++ clone_repos.py
@@ -1,15 +1,18 @@
 import os
 
-# File containing repository URLs
 repo_file = "templates-repos.txt"
 
-# Directory to clone into
 clone_dir = "community-templates"
 
-# Read repository URLs from file
 with open(repo_file, 'r') as file:
     repos = [line.strip() for line in file if line.strip()]
 
-# Clone each repository
+print("***************************************************")
+print("Nuclei Template Cloning Script")
+print("GitHub: Shubham-rooter")
+print("***************************************************")
+
+print("Script by Shubham-rooter")
+
 for repo in repos:
     os.system(f"git clone {repo} {clone_dir}/{os.path.basename(repo)}")
